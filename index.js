eb7540b2bef3c81546ee48c25db54d596d28303d
Megan Adams <meganoliviawendyadams@gmail.com>
2019-05-29 23:58:09 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+ 
+
+dwarves = ["Doc", "Dopey", "Bashful", "Grumpy"];
+planeteerCalls = ["earth", "wind", "fire", "water", "heart"];
+shortWords = ["wind", "fire"];
+assortedWords = ["earth", "wind", "heart", "fire"]; 
+
+
 function dwarfRollCall(dwarves) {
+  var dwarveCount = '';
+  
+  
+  for(let i = 0; i < dwarves.length; i++) {
+    dwarveCount += `${i + 1}. ${dwarves[i]} `
+  }
+  return dwarveCount;
 }
 
-function summonCaptainPlanet(planeteerCalls){
-}
+function summonCaptainPlanet(array){
+  return array.map(function(item){
+		return item.toUpperCase() + "!";
+	});
+}	
+summonCaptainPlanet(planeteerCalls);
+
 
 function longPlaneteerCalls(words) {
+  words.some(longPlaneteerCalls);
+function longPlaneteerCalls(items){
+ return items.length > 4;
+}	
 }
 
-function findTheCheese (foods) {
-}
+
